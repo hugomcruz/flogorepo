fd70f9c2db735537233a792a912eec11df2caae0
Hugo Cruz <hugomcruz@users.noreply.github.com>
2021-03-23 20:15:30 +0700

    New radar process single line

---
+++ radarline/activity.go
@@ -0,0 +1,71 @@
+package radarline
+
+import (
+	"sync"
+
+	"github.com/TIBCOSoftware/flogo-lib/core/activity"
+)
+
+// log is the default package logger
+//var log = logger.GetLogger("tibco-activity-radar")
+
+type Output struct {
+	msgType      string
+	timestamp    int64
+	icaoHexCode  string
+	callsign     string
+	altitude     int32
+	latitude     float32
+	longitude    float32
+	onGround     int32
+	groundSpeed  float32
+	track        float32
+	verticalRate int32
+}
+
+type Input struct {
+	payload []byte
+}
+
+const (
+	ivCounterName = "counterName"
+	ivIncrement   = "increment"
+	ivReset       = "reset"
+
+	ovValue = "value"
+)
+
+// CounterActivity is a Counter Activity implementation
+type CounterActivity struct {
+	sync.Mutex
+	metadata *activity.Metadata
+	counters map[string]int
+}
+
+// NewActivity creates a new CounterActivity
+func NewActivity(metadata *activity.Metadata) activity.Activity {
+	return &CounterActivity{metadata: metadata, counters: make(map[string]int)}
+}
+
+// Metadata implements activity.Activity.Metadata
+func (a *CounterActivity) Metadata() *activity.Metadata {
+	return a.metadata
+}
+
+// Eval implements activity.Activity.Eval
+func (a *CounterActivity) Eval(context activity.Context) (done bool, err error) {
+
+	linedata := context.GetInput("linedata").(Output)
+
+	if err != nil {
+		return true, err
+	}
+
+	//log.Debugf("Input: %s", data)
+
+	context.SetOutput("msgType", linedata.msgType)
+	context.SetOutput("callsign", linedata.callsign)
+	context.SetOutput("icaohex", linedata.icaoHexCode)
+
+	return true, nil
+}
